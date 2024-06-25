a777f5cc6869c03eaf3fa51906897a6808e07858
nic00la <nic00la.kaleta@gmail.com>
2024-06-25 13:33:44 +0200

    Add Country - xUnit Test - Part 1

---
+++ Entities/Country.cs
@@ -0,0 +1,7 @@
+﻿namespace Entities;
+
+public class Country
+{
+    public Guid Id { get; set; }
+    public string? Name { get; set; }
+}
---
+++ ServiceContracts/DTO/CountryAddRequest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities;
+
+namespace ServiceContracts.DTO;
+
+/// <summary>
+/// DTO class for adding a new country
+/// </summary>
+public class CountryAddRequest
+{
+    public string? CountryName { get; set; }
+
+    public Country ToCountry()
+    {
+        return new Country()
+        {
+            CountryName = CountryName
+        };
+    }
+}
