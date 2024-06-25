6a182d203f9454b01f6f40208b9d61e11b2ecac6
nic00la <nic00la.kaleta@gmail.com>
2024-06-25 14:35:51 +0200

    AddCountry() - Validation

--- Services/CountriesService.cs
+++ Services/CountriesService.cs
@@ -15,6 +15,23 @@ public class CountriesService : ICountriesService
 
     public CountryResponse AddCountry(CountryAddRequest? countryAddRequest)
     {
+        // Validation: countryAddRequest should not be null
+        if (countryAddRequest == null)
+            throw new ArgumentNullException(nameof(countryAddRequest));
+
+        // Validation: CountryName can't be null
+        if (countryAddRequest.CountryName == null)
+            throw new ArgumentException(
+                nameof(countryAddRequest.CountryName));
+
+        // Validation: CountryName can't be duplicate
+        if (_countries
+                .Where(temp =>
+                    temp.CountryName == countryAddRequest.CountryName).Count() >
+            0)
+            throw new ArgumentException(
+                $"Country with name {countryAddRequest.CountryName} already exists");
+
         // Convert object from CountryAddRequest to Country type
         Country country = countryAddRequest.ToCountry();
 
