300bd860c3b82d948b4e711b33395366368b5117
nic00la <nic00la.kaleta@gmail.com>
2024-06-25 13:49:45 +0200

    Add Country - xUnit Tests - Part 2

---
+++ CRUDTests/CountriesServiceTest.cs
@@ -0,0 +1,12 @@
+﻿using ServiceContracts;
+
+namespace CRUDTests;
+
+public class CountriesServiceTest
+{
+    private readonly ICountriesService _countriesService;
+
+    public CountriesServiceTest()
+    {
+    }
+}
--- ServiceContracts/ICountriesService.cs
+++ ServiceContracts/ICountriesService.cs
@@ -1,5 +1,18 @@
-﻿namespace ServiceContracts;
+﻿using ServiceContracts.DTO;
 
+namespace ServiceContracts;
+
+/// <summary>
+/// Represents business logic for manipulating
+/// Country entities
+/// </summary>
 public interface ICountriesService
 {
+    /// <summary>
+    /// Adds a new country object to the list of countries
+    /// </summary>
+    /// <param name="countryAddRequest">Country object to add</param>
+    /// <returns>Returns the country object after adding it
+    /// (including newly generated country id)</returns>
+    CountryResponse AddCountry(CountryAddRequest? countryAddRequest);
 }
