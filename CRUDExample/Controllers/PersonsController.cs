e55ceea19d71da952ab82f537f6eec886ecdeb0b
nic00la <nic00la.kaleta@gmail.com>
2024-07-03 17:32:57 +0200

    Getting Started with UI ✨😎

---
+++ CRUDExample/Controllers/PersonsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CRUDExample.Controllers;
+
+public class PersonsController : Controller
+{
+    [Route("persons/index")]
+    [Route("/")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
