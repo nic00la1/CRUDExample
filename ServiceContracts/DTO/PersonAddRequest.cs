3d827ebdeaafcf7f98c9fd424d9530e7f7b779f9
nic00la <nic00la.kaleta@gmail.com>
2024-07-11 22:40:57 +0200

    Input Tag Helpers - Part 1 ✅✨

--- ServiceContracts/DTO/PersonAddRequest.cs
+++ ServiceContracts/DTO/PersonAddRequest.cs
@@ -14,12 +14,15 @@ public class PersonAddRequest
 
     [Required(ErrorMessage = "Email can't be blank")]
     [EmailAddress(ErrorMessage = "Email value should be a valid email")]
+    [DataType(DataType.EmailAddress)]
     public string? Email { get; set; }
 
-    public DateTime? DateOfBirth { get; set; }
+    [DataType(DataType.Date)] public DateTime? DateOfBirth { get; set; }
+
     public GenderOptions? Gender { get; set; }
     public Guid? CountryID { get; set; }
     public string? Address { get; set; }
+
     public bool ReceiveNewsLetters { get; set; }
 
     /// <summary>
