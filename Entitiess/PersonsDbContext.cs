5c5a9e4412b576b4c3b4b3c3125bad83e46573c5
nic00la <nic00la.kaleta@gmail.com>
2024-07-23 22:28:24 +0200

    Seed Data ✨

--- Entitiess/PersonsDbContext.cs
+++ Entitiess/PersonsDbContext.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 
@@ -18,5 +19,23 @@ public class PersonsDbContext : DbContext
 
         modelBuilder.Entity<Country>().ToTable("Countries");
         modelBuilder.Entity<Person>().ToTable("Persons");
+
+        // Seed data to countries table
+        string countriesJson = File.ReadAllText("countries.json");
+
+        List<Country> countries =
+            JsonSerializer.Deserialize<List<Country>>(countriesJson);
+
+        foreach (Country country in countries)
+            modelBuilder.Entity<Country>().HasData(country);
+
+        // Seed data to persons table
+        string personsJson = File.ReadAllText("persons.json");
+
+        List<Person> persons =
+            JsonSerializer.Deserialize<List<Person>>(personsJson);
+
+        foreach (Person person in persons)
+            modelBuilder.Entity<Person>().HasData(person);
     }
 }
