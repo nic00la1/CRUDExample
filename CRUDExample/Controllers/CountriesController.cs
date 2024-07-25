dcf7a899985217d890848fac0c8d1fc222fad8b9
nic00la <nic00la.kaleta@gmail.com>
2024-07-25 13:10:45 +0200

    Excel to Database Upload - Part 2 ✅🆙

---
+++ CRUDExample/Controllers/CountriesController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CRUDExample.Controllers;
+
+[Route("[controller]")]
+public class CountriesController : Controller
+{
+    [Route("[action]")]
+    public IActionResult UploadFromExcel()
+    {
+        return View();
+    }
+}
